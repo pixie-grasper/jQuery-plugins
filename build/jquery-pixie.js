ed5feeb66b640def8bcdd9770c1d4f6129646ddf
pixie-grasper <himajinn13sei@gmail.com>
2016-12-02 21:37:20 +0900

    implemented atlas::zoom-function

--- build/jquery-pixie.js
+++ build/jquery-pixie.js
@@ -370,6 +370,11 @@
       // jQuery Plugin: pixie::atlas
 
       const default_options = {
+        overlap_elements: function(width, height) {
+          return [];
+        },
+        update_overlaps: function(x, y, scale) {
+        },
       };
 
       const create_box = function($) {
@@ -409,6 +414,8 @@
             this._.appendChild(element[0]);
           } else if (element instanceof SVG) {
             this._.appendChild(element._);
+          } else {
+            this._.appendChild(element);
           }
           return this;
         };
@@ -425,6 +432,26 @@
         return new SVG(tagname);
       };
 
+      const wheel_event_name = function() {
+        if ('onwheel' in document) {
+          return 'wheel';
+        } else if ('onmousewheel' in document) {
+          return 'mousewheel';
+        } else {
+          return 'DOMMouseScroll';
+        }
+      };
+
+      const wheel_direction = function(event) {
+        if (event.originalEvent.deltaY) {
+          return -event.originalEvent.deltaY;
+        } else if (event.originalEvent.wheelDelta) {
+          return event.originalEvent.wheelDelta;
+        } else {
+          return event.originalEvent.detail;
+        }
+      };
+
       const atlas_init = function() {
         const box = create_box(this.$);
         this.svg = {
@@ -437,28 +464,54 @@
             height: box.height,
           }),
         };
-        this.svg._.appendTo(this.$);
+        this.svg.$ = $(this.svg._._).appendTo(this.$);
+        this.overlap_elements = this.options.overlap_elements(box.width, box.height);
         const this_ = this;
         let dragging = false;
         let drag_start_pos_x = null;
         let drag_start_pos_y = null;
         let init_pos_x = null;
         let init_pos_y = null;
-        $(this.svg._._).mousedown(function(event) {
+        this.svg.$.mousedown(function(event) {
           drag_start_pos_x = event.clientX;
           drag_start_pos_y = event.clientY;
           init_pos_x = this_.x;
           init_pos_y = this_.y;
           dragging = true;
+        }).on(wheel_event_name(), function(event) {
+          const box = create_box(this_.$);
+          const scale = this_.scale;
+          const scale_inv = 1 / scale;
+          const scale_base = this_.scale_base;
+          const scale_base_inv = 1 / scale_base;
+          const x = this_.x + event.clientX * scale_inv;
+          const y = this_.y + (box.height - event.clientY) * scale_inv;
+          if (wheel_direction(event) < 0) {
+            this_.scale *= scale_base_inv;
+            this_.x = x - (x - this_.x) * scale_base;
+            this_.y = y - (y - this_.y) * scale_base;
+          } else {
+            this_.scale *= scale_base;
+            this_.x = x - (x - this_.x) * scale_base_inv;
+            this_.y = y - (y - this_.y) * scale_base_inv;
+          }
+          this_.show();
+          this_.options.update_overlaps(x, y, this_.scale);
         });
         $(document).mousemove(function(event) {
           if (dragging) {
             const delta_x = event.clientX - drag_start_pos_x;
             const delta_y = event.clientY - drag_start_pos_y;
-            this_.x = init_pos_x - delta_x;
-            this_.y = init_pos_y + delta_y;
+            this_.x = init_pos_x - delta_x / this_.scale;
+            this_.y = init_pos_y + delta_y / this_.scale;
             this_.show();
           }
+          const box = create_box(this_.$);
+          const scale = this_.scale;
+          const scale_inv = 1 / scale;
+          const x = this_.x + event.clientX * scale_inv;
+          const y = this_.y + (box.height - event.clientY) * scale_inv;
+          this_.options.update_overlaps(x, y, scale);
         }).mouseup(function(event) {
           dragging = false;
         });
@@ -487,7 +540,7 @@
           const box = create_box(this.$);
           const scale = this.scale;
           const scale_inv = 1 / scale;
-          let rendering = [
+          const rendering = [
             this.x,
             this.y,
             this.x + box.width * scale_inv,
@@ -513,6 +566,7 @@
             this.g = {
               _: _('g'),
             };
+            this.g.$ = $(this.g._._);
             this.g._.appendTo(this.svg._.empty());
             this.cache_SVG_list = [];
             const list = this.cache_id_list;
@@ -531,7 +585,7 @@
                     r: item.r,
                   };
                   this.cache_SVG_list[i] = svg;
-                  svg._.appendTo(this.g._);
+                  svg._.appendTo(this.g.$);
                   break;
                 }
                 default: {
@@ -539,6 +593,9 @@
                   break;
                 }
               }
+            }
+            for (let i = 0; i < this.overlap_elements.length; i++) {
+              this.g._.append(this.overlap_elements[i]);
             }
           }
           const svg_list = this.cache_SVG_list;
@@ -578,6 +635,7 @@
         x: 0,
         y: 0,
         scale: 1,
+        scale_base: 1.3,
       };
 
       return function(...args) {
--- pixie/atlas.js
+++ pixie/atlas.js
@@ -1,6 +1,11 @@
 // jQuery Plugin: pixie::atlas
 
 const default_options = {
+  overlap_elements: function(width, height) {
+    return [];
+  },
+  update_overlaps: function(x, y, scale) {
+  },
 };
 
 const create_box = function($) {
@@ -40,6 +45,8 @@ const SVG = function(tagname) {
       this._.appendChild(element[0]);
     } else if (element instanceof SVG) {
       this._.appendChild(element._);
+    } else {
+      this._.appendChild(element);
     }
     return this;
   };
@@ -56,6 +63,26 @@ const _ = function(tagname) {
   return new SVG(tagname);
 };
 
+const wheel_event_name = function() {
+  if ('onwheel' in document) {
+    return 'wheel';
+  } else if ('onmousewheel' in document) {
+    return 'mousewheel';
+  } else {
+    return 'DOMMouseScroll';
+  }
+};
+
+const wheel_direction = function(event) {
+  if (event.originalEvent.deltaY) {
+    return -event.originalEvent.deltaY;
+  } else if (event.originalEvent.wheelDelta) {
+    return event.originalEvent.wheelDelta;
+  } else {
+    return event.originalEvent.detail;
+  }
+};
+
 const atlas_init = function() {
   const box = create_box(this.$);
   this.svg = {
@@ -68,28 +95,54 @@ const atlas_init = function() {
       height: box.height,
     }),
   };
-  this.svg._.appendTo(this.$);
+  this.svg.$ = $(this.svg._._).appendTo(this.$);
+  this.overlap_elements = this.options.overlap_elements(box.width, box.height);
   const this_ = this;
   let dragging = false;
   let drag_start_pos_x = null;
   let drag_start_pos_y = null;
   let init_pos_x = null;
   let init_pos_y = null;
-  $(this.svg._._).mousedown(function(event) {
+  this.svg.$.mousedown(function(event) {
     drag_start_pos_x = event.clientX;
     drag_start_pos_y = event.clientY;
     init_pos_x = this_.x;
     init_pos_y = this_.y;
     dragging = true;
+  }).on(wheel_event_name(), function(event) {
+    const box = create_box(this_.$);
+    const scale = this_.scale;
+    const scale_inv = 1 / scale;
+    const scale_base = this_.scale_base;
+    const scale_base_inv = 1 / scale_base;
+    const x = this_.x + event.clientX * scale_inv;
+    const y = this_.y + (box.height - event.clientY) * scale_inv;
+    if (wheel_direction(event) < 0) {
+      this_.scale *= scale_base_inv;
+      this_.x = x - (x - this_.x) * scale_base;
+      this_.y = y - (y - this_.y) * scale_base;
+    } else {
+      this_.scale *= scale_base;
+      this_.x = x - (x - this_.x) * scale_base_inv;
+      this_.y = y - (y - this_.y) * scale_base_inv;
+    }
+    this_.show();
+    this_.options.update_overlaps(x, y, this_.scale);
   });
   $(document).mousemove(function(event) {
     if (dragging) {
       const delta_x = event.clientX - drag_start_pos_x;
       const delta_y = event.clientY - drag_start_pos_y;
-      this_.x = init_pos_x - delta_x;
-      this_.y = init_pos_y + delta_y;
+      this_.x = init_pos_x - delta_x / this_.scale;
+      this_.y = init_pos_y + delta_y / this_.scale;
       this_.show();
     }
+    const box = create_box(this_.$);
+    const scale = this_.scale;
+    const scale_inv = 1 / scale;
+    const x = this_.x + event.clientX * scale_inv;
+    const y = this_.y + (box.height - event.clientY) * scale_inv;
+    this_.options.update_overlaps(x, y, scale);
   }).mouseup(function(event) {
     dragging = false;
   });
@@ -118,7 +171,7 @@ const atlas = {
     const box = create_box(this.$);
     const scale = this.scale;
     const scale_inv = 1 / scale;
-    let rendering = [
+    const rendering = [
       this.x,
       this.y,
       this.x + box.width * scale_inv,
@@ -144,6 +197,7 @@ const atlas = {
       this.g = {
         _: _('g'),
       };
+      this.g.$ = $(this.g._._);
       this.g._.appendTo(this.svg._.empty());
       this.cache_SVG_list = [];
       const list = this.cache_id_list;
@@ -162,7 +216,7 @@ const atlas = {
               r: item.r,
             };
             this.cache_SVG_list[i] = svg;
-            svg._.appendTo(this.g._);
+            svg._.appendTo(this.g.$);
             break;
           }
           default: {
@@ -170,6 +224,9 @@ const atlas = {
             break;
           }
         }
+      }
+      for (let i = 0; i < this.overlap_elements.length; i++) {
+        this.g._.append(this.overlap_elements[i]);
       }
     }
     const svg_list = this.cache_SVG_list;
@@ -209,6 +266,7 @@ const atlas = {
   x: 0,
   y: 0,
   scale: 1,
+  scale_base: 1.3,
 };
 
 return function(...args) {
